a251aeb1b36d0e236a9b835c7cb1afac8e1b04d9
Marcel Valdez <marcel.valdez@live.com>
2013-07-14 17:20:20 -0400

    Finished simple language (until now: 1790 of 13951, 13%)

--- lib/simple_language/expressions.rb
+++ lib/simple_language/expressions.rb
@@ -23,11 +23,11 @@ class Add < Struct.new(:left, :right)
     
     def sign; '+'; end    
     
-    def reduce
+    def reduce(environment)
         if left.reducible?
-            Add.new(left.reduce, right)            
+            Add.new(left.reduce(environment), right)            
         elsif right.reducible?
-            Add.new(left, right.reduce)
+            Add.new(left, right.reduce(environment))
         else
             Number.new(left.value + right.value)
         end
@@ -41,11 +41,11 @@ class Multiply < Struct.new(:left, :right)
     
     #  Reduce always builds a new expression rather than modifying an existing
     #  one.
-    def reduce
+    def reduce(environment)
         if left.reducible?
-            Multiply.new(left.reduce, right)            
+            Multiply.new(left.reduce(environment), right)
         elsif right.reducible?
-            Multiply.new(left, right.reduce)
+            Multiply.new(left, right.reduce(environment))
         else
             Number.new(left.value * right.value)
         end
@@ -61,29 +61,121 @@ class LessThan < Struct.new(:left, :right)
     
     def sign; '<' end
     
-    def reduce
+    def reduce(environment)
         if left.reducible?
-            LessThan.new(left.reduce, right)
+            LessThan.new(left.reduce(environment), right)
         elsif right.reducible?
-            LessThan.new(left, right.reduce)
+            LessThan.new(left, right.reduce(environment))
         else
             Boolean.new(left.value < right.value)
         end
     end
 end
 
-class Machine < Struct.new(:expression)
+class Variable < Struct.new(:name)
+    def to_s; name.to_s end
+    def inspect; "<#{self}>" end
+    def reducible?; true end
+    
+    # environment contains the values for each variable name
+    def reduce(environment)
+        environment[name]
+    end
+end
+
+class DoNothing
+    def to_s; 'do-nothing' end
+    def inspect; "<#{self}>" end
+    def reducible?; false end
+       
+    def ==(other_statement)
+        other_statement.instance_of? DoNothing
+    end    
+end
+
+class Assign < Struct.new(:name, :expression)
+    def to_s; "#{name} = #{expression}" end
+    def inspect; "<#{self}>" end
+    def reducible?; true end
+    
+    def reduce(env)
+        if expression.reducible?
+            return Assign.new(name, expression.reduce(env)), env
+        else
+        return DoNothing.new, env.merge({ name => expression })
+        end
+    end
+end
+
+class While < Struct.new(:condition, :body)
+    def to_s
+        "while (#{condition}) { #{body} }"
+    end
+    
+    def inspect; "<#{self}>" end
+    
+    def reducible?; true end
+        
+    def reduce(env)
+        return If.new(condition, Sequence.new(body, self), DoNothing.new), env
+    end
+end
+
+class If < Struct.new(:condition, :consequence, :alternative)
+    def to_s
+        "if (#{condition}) { #{consequence} } else { #{alternative} }"
+    end
+    
+    def inspect; "<#{self}>" end
+    def reducible?; true end
+    
+    def reduce(env)
+        if condition.reducible?
+            return If.new(condition.reduce(env), consequence, alternative), env
+        else
+            case condition
+            when Boolean.new(true)
+                return consequence, env
+            when Boolean.new(false)
+                return alternative, env
+            end
+        end
+    end
+end
+
+class Sequence < Struct.new(:first, :second)
+    def to_s
+        "#{first}; #{second}"
+    end
+    
+    def inspect; "<#{self}>" end
+        
+    def reducible?; true end
+    
+    def reduce(env)
+        case first
+        when DoNothing.new
+            return second, env
+        else
+            reduced_first, reduced_env = first.reduce(env)
+            return Sequence.new(reduced_first, second), reduced_env
+        end
+    end
+end
+
+class Machine < Struct.new(:expression, :environment)
     # perform a step in expression reduction
     def step
-        self.expression = expression.reduce
+        self.expression, self.environment = expression.reduce(environment)
     end
     
     def run
         while expression.reducible?
-            puts expression
+            puts "#{expression} -- #{environment}"
             step
         end
         
-        return expression
+        puts "#{expression} -- #{environment}"
+        return expression, environment
     end
 end
--- test/simple_language/expressions_test.rb
+++ test/simple_language/expressions_test.rb
@@ -4,15 +4,16 @@ require_relative '../../lib/simple_language/expressions.rb'
 
 describe Machine do
     before do 
-        @machine = Machine.new
+        @machine = Machine.new(nil, nil)
     end
 
     it 'should reduce an expression' do
         # arrange        
         obj = make_reducible     
-        
+        environment = nil
         # act
-        result = Machine.new(obj).run
+        environment = nil
+        result, env = Machine.new(obj, nil).run
         # assert
         result[:reduced].must_equal true
     end
@@ -24,7 +25,8 @@ describe Machine do
             false
         end        
         # act
-        result = Machine.new(obj).run
+        environment = nil
+        result, new_env = Machine.new(obj, environment).run
         # assert
         result.must_equal obj
     end
@@ -35,11 +37,99 @@ describe Machine do
             Multiply.new(Number.new(1), Number.new(2)),
             Multiply.new(Number.new(3), Number.new(4)))
         # act
-        result = Machine.new(expression).run
+        environment = nil
+        result, new_env = Machine.new(expression, environment).run
+        # assert        
+        result.must_equal Number.new(14)
+    end
+    
+    it 'should return the value of a variable' do
+        # arrange
+        environment = { x: Number.new(3) }
+        expression = Variable.new(:x)
+        # act
+        result, new_env = Machine.new(expression, environment).run
         # assert
-        result.reducible?.must_equal false
-        result.class.must_equal Number
-        result.value.must_equal 14
+        result.must_equal environment[:x]
+    end
+    
+    it 'should reduce a statement' do
+        # arrange
+        statement = Assign.new(:x, Add.new(Variable.new(:x), Number.new(1)))
+        env = { x: Number.new(2) }
+        # act
+        expression, new_env = Machine.new(statement, env).run
+        # assert
+        expression.must_equal DoNothing.new
+        new_env[:x].must_equal Number.new(3)
+    end
+    
+    it 'should evaluate conditionals' do
+        # arrange
+        conditional = If.new(Variable.new(:x), 
+                        Assign.new(:y, Number.new(1)), 
+                        DoNothing.new)
+        env = { x: Boolean.new(false) }
+        # act
+        expr, new_env = Machine.new(conditional, env).run
+        # assert
+        expr.must_equal DoNothing.new
+        new_env[:y].must_be_nil
+    end
+    
+    it 'should perform while loops' do
+        # arrange
+        while_expr = While.new(
+                LessThan.new(Variable.new(:x), Number.new(5)),
+                Assign.new(:x, Multiply.new(
+                                    Variable.new(:x), Number.new(3))))
+        env = { x: Number.new(1) }
+        machine = Machine.new(while_expr, env)
+        # act
+        expr, env = machine.run
+        # assert
+        env[:x].must_equal Number.new(9)
+    end
+end
+
+describe Sequence do
+    it 'should ignore the first DoNothing' do
+        # arrange
+        env = {}
+        target = Sequence.new(DoNothing.new, "test")
+        # act
+        expr, new_env = target.reduce(env)
+        # assert
+        expr.must_equal "test"
+        new_env.must_equal env
+    end
+    
+    it 'should reduce the first expression' do
+        # arrange
+        env = {}
+        expr = make_reducible
+        target = Sequence.new(expr, nil)
+        # act
+        new_expr, new_env = target.reduce(env)
+        # assert
+        expr[:reduced].must_equal true
+        new_expr.must_be_instance_of Sequence
+    end
+end
+
+describe While do
+    it 'should reduce to an If Expression' do
+        # arrange
+        condition = "condition"
+        target = While.new(condition, nil)
+        # act
+        expr, env = target.reduce({})
+        # assert
+        env.must_equal({})
+        expr.condition.must_equal condition
+        expr.must_be_instance_of If
+        expr.consequence.must_be_instance_of Sequence
+        expr.alternative.must_be_instance_of DoNothing
     end
 end
 
@@ -69,7 +159,8 @@ describe Multiply do
         two = Number.new(2)
         three = Number.new(3)
         # act
-        result = Multiply.new(two, three).reduce
+        environment = nil
+        result = Multiply.new(two, three).reduce(environment)
         # assert
         result.value.must_equal 6
     end
@@ -81,7 +172,8 @@ describe Multiply do
         
         target = Multiply.new(left, right)
         # act
-        target.reduce.reduce
+        environment = nil
+        target.reduce(environment).reduce(environment)
         # assert
         left[:reduced].must_equal true
         right[:reduced].must_equal true
@@ -91,6 +183,59 @@ end
 describe LessThan do
     it 'should be reducible' do
         LessThan.new(nil, nil).reducible?.must_equal true
+    end    
+end
+
+describe Assign do
+    it 'should set values' do
+        # arrange
+        target = Assign.new(:x, Number.new(1))
+        # act
+        expression, environment = target.reduce({})
+        # assert
+        expression.must_equal DoNothing.new
+        environment[:x].must_equal Number.new(1)
+    end
+    
+    it 'should reduce expressions' do
+        # arrange
+        expression = make_reducible
+        target = Assign.new(:x, expression)
+        environment = {}
+        # act
+        expression, new_environment = target.reduce(environment)
+        # assert
+        expression.must_be_instance_of Assign
+        new_environment.must_equal environment        
+    end
+end
+
+describe If do
+    it 'should reduce the condition' do
+        # arrange
+        reducible = make_reducible
+        # act
+        result, env = If.new(reducible, nil, nil).reduce({})
+        # assert
+        reducible[:reduced].must_equal true
+    end
+    
+    it 'should go to consequence on Boolean true' do
+        # arrange
+        consequence = "test"
+        # act
+        result, env = If.new(Boolean.new(true), consequence, nil).reduce({})
+        # assert
+        result.must_equal "test"
+    end
+    
+    it 'should return alternative on Boolean false' do
+        # arrange
+        alternative = "test"
+        # act
+        result, env = If.new(Boolean.new(false), nil, alternative).reduce({})
+        # assert
+        result.must_equal "test"        
     end
 end
 
@@ -98,7 +243,8 @@ def make_reducible
     obj = { reduced: false }
     
     def obj.reducible?; not self[:reduced] end
-    def obj.reduce
+        
+    def obj.reduce(environment)
         self[:reduced] = true
         self
     end
